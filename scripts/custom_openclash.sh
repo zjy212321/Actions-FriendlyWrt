a4f5c8e31e44c936db859dfdd940837bb69f1786
zjy212321 <zjy212321@gmail.com>
2025-03-21 20:30:09 +0800

    workflow

--- scripts/custom_openclash.sh
+++ scripts/custom_openclash.sh
@@ -2,17 +2,22 @@
 echo ">>> Setting up OpenClash..."
 
 # Install openclash
-(cd friendlywrt/package && {
-git clone --depth 1 -b master --single-branch https://github.com/vernesong/OpenClash.git 
-mv OpenClash/luci-app-openclash ./luci-app-openclash
-rm -rf OpenClash
+(cd friendlywrt && {
+    echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
+    ./scripts/feeds update openclash
+    ./scripts/feeds install -a -p openclash
 })
+cat >> configs/rockchip/01-nanopi <<EOL
+CONFIG_PACKAGE_luci-app-openclash=y
+CONFIG_PACKAGE_luci-compat=y
+EOL
+
 
 # Install openclash core
 ( cd friendlywrt/package && {
 mkdir -p base-files/files/etc/openclash/core
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-armv8.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep $1 | awk -F '"' '{print $4}')
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep armv8 | awk -F '"' '{print $4}')
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-armv8.tar.gz"
 wget -qO- $CLASH_DEV_URL | tar xOvz > base-files/files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > base-files/files/etc/openclash/core/clash_tun
