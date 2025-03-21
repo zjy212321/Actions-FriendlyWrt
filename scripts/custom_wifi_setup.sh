419045665ddcbdd70f6bfc033358a1f88db201a1
zjy212321 <zjy212321@gmail.com>
2025-03-21 11:48:51 +0800

    workflow

--- scripts/custom_wifi_setup.sh
+++ scripts/custom_wifi_setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 echo ">>> Setting default WiFi..."
+mkdir -p friendlywrt/package/base-files/files/etc/config
 WIRELESS_CFG="friendlywrt/package/base-files/files/etc/config/wireless"
 
 cat > ${WIRELESS_CFG} <<EOF
 config wifi-device 'radio0'
     option type 'mac80211'
-    option channel '11'
-    option hwmode '11g'
-    option path 'platform/ff160000.wifi'
-    option htmode 'HT20'
-    option disabled '0'
+    option channel '161'
+    option path 'platform/ff5f0000.dwmmc/mmc_host/mmc1:0001/mmc1:0001:1'
+    option htmode 'VHT80'
+    option country 'US'
+    option cell_density '0'
 
 config wifi-iface 'default_radio0'
     option device 'radio0'
     option network 'lan'
     option mode 'ap'
-    option ssid 'FriendlyWrt'
+    option ssid 'UrekaGo-5G'
     option encryption 'psk2'
-    option key 'yourpassword'
+    option key 'password'
 EOF
