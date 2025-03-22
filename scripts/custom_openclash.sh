cc4f0463725eb312f690ca39328da33afeb76c35
zjy212321 <zjy212321@gmail.com>
2025-03-22 09:44:50 +0800

    workflow

--- scripts/custom_openclash.sh
+++ scripts/custom_openclash.sh
@@ -14,22 +14,30 @@ EOL
 
 
 # Install openclash core
-( cd friendlywrt/package && {
-mkdir -p base-files/files/etc/openclash/core
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-armv8.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep armv8 | awk -F '"' '{print $4}')
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-armv8.tar.gz"
-wget -qO- $CLASH_DEV_URL | tar xOvz > base-files/files/etc/openclash/core/clash
-wget -qO- $CLASH_TUN_URL | gunzip -c > base-files/files/etc/openclash/core/clash_tun
-wget -qO- $CLASH_META_URL | tar xOvz > base-files/files/etc/openclash/core/clash_meta
-chmod +x base-files/files/etc/openclash/core/clash*
-})
+# ( cd friendlywrt/package && {
+# mkdir -p base-files/files/etc/openclash/core
+# CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/v0.45.35-beta/core-lateset/dev/clash-linux-armv8.tar.gz"
+# CLASH_TUN_URL="https://github.com/vernesong/OpenClash/raw/v0.45.35-beta/core-lateset/premium/clash-linux-armv8-2022.06.19-13-ga45638d.gz"
+# CLASH_META_URL="https://github.com/vernesong/OpenClash/raw/v0.45.35-beta/core-lateset/meta/clash-linux-armv8.tar.gz"
+# wget -qO- $CLASH_DEV_URL | tar xOvz > base-files/files/etc/openclash/core/clash
+# wget -qO- $CLASH_TUN_URL | gunzip -c > base-files/files/etc/openclash/core/clash_tun
+# wget -qO- $CLASH_META_URL | tar xOvz > base-files/files/etc/openclash/core/clash_meta
+# chmod +x base-files/files/etc/openclash/core/clash*
+# })
+mv ../scripts/openclash-config/clash_v0.45.35 friendlywrt/package/base-files/files/etc/clash_v0.45.35
+mv ../scripts/openclash-config/clash_tun_v0.45.35 friendlywrt/package/base-files/files/etc/clash_tun_v0.45.35
+mv ../scripts/openclash-config/clash_meta_v0.45.35 friendlywrt/package/base-files/files/etc/clash_meta_v0.45.35
+
+
 
 # Set default openclash config
 mv ../scripts/openclash-config/openclash friendlywrt/package/base-files/files/etc/openclash.config
 mv ../scripts/openclash-config/config.yaml friendlywrt/package/base-files/files/etc/openclash.config.yaml
 OPENCLASH_CFG="friendlywrt/package/base-files/files/etc/uci-defaults/99_openclash"
 cat > ${OPENCLASH_CFG} <<EOF
+mv -f /etc/clash_v0.45.35 /etc/openclash/core/clash
+mv -f /etc/clash_tun_v0.45.35 /etc/openclash/core/clash_tun
+mv -f /etc/clash_meta_v0.45.35 /etc/openclash/core/clash_meta
 mv -f /etc/openclash.config /etc/config/openclash
 mv -f /etc/openclash.config.yaml /etc/openclash/config/config.yaml
 /etc/init.d/openclash restart
