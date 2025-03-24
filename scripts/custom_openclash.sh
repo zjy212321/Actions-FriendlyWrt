4a9896a0362b21ef25bbeb95a6c3d01d5b542abf
zjy212321 <zjy212321@gmail.com>
2025-03-24 14:42:11 +0800

    f

--- scripts/custom_openclash.sh
+++ scripts/custom_openclash.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 echo ">>> Setting up OpenClash..."
 
-# Install openclash
-(cd friendlywrt && {
-    echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default    
-    ./scripts/feeds update openclash
-    ./scripts/feeds install -a -p openclash
-})
-cat >> configs/rockchip/01-nanopi <<EOL
-CONFIG_PACKAGE_luci-app-openclash=y
-CONFIG_PACKAGE_luci-compat=y
-EOL
-
+ 
 
 # Install openclash core
 # ( cd friendlywrt/package && {
@@ -33,6 +23,8 @@ mv ../scripts/openclash-config/clash_meta friendlywrt/package/base-files/files/e
 # Set default openclash config
 mv ../scripts/openclash-config/openclash friendlywrt/package/base-files/files/etc/openclash.config
 mv ../scripts/openclash-config/config.yaml friendlywrt/package/base-files/files/etc/openclash.config.yaml
+mv ../scripts/openclash-config/check_start_theme_openclash.sh friendlywrt/package/base-files/files/etc/init.d/check_start_theme_openclash.sh
+
 OPENCLASH_CFG="friendlywrt/package/base-files/files/etc/uci-defaults/99_openclash"
 cat > ${OPENCLASH_CFG} <<EOF
 mv -f /etc/clash_tmp /etc/openclash/core/clash
@@ -44,4 +36,6 @@ mv -f /etc/openclash.config.yaml /etc/openclash/config/config.yaml
 uci set openclash.config.enable=1
 uci commit openclash
 /etc/init.d/openclash start
+chmod +x /etc/init.d/check_start_theme_openclash.sh
+ln -s /etc/init.d/check_start_theme_openclash.sh /etc/rc.d/S99check_openclash
 EOF
