e7f1c7a10ab70dec93b2aa94e130daffad2e13d7
zjy212321 <zjy212321@gmail.com>
2025-03-24 14:14:18 +0800

    f

---
+++ scripts/openclash-config/check_start_theme_openclash.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# 检查当前主题
+CURRENT_THEME=$(uci get luci.main.mediaurlbase | awk -F '/' '{print $NF}')
+# 若当前主题不是 Argon，将其设置为 Argon
+if [ "$CURRENT_THEME" != "argon" ]; then
+    uci set luci.main.mediaurlbase='/luci-static/argon'
+    uci commit luci
+fi
+
+
+# 定义检查和启动 OpenClash 的函数
+check_and_start_openclash() {
+    while true; do
+        # 检查 OpenClash 服务状态
+        if /etc/init.d/openclash status | grep -q 'running'; then
+            echo "OpenClash is running."
+        else
+            echo "OpenClash is not running. Starting OpenClash..."
+            uci set openclash.config.enable=1
+            uci commit openclash
+            /etc/init.d/openclash start
+        fi
+        # 每隔 60 秒检查一次，可以根据需要调整间隔时间
+        sleep 10
+    done
+}
+
+# 调用函数
+check_and_start_openclash  
