b614f0bf038dcbe6a186b1340f3a856a989daf2b
zjy212321 <zjy212321@gmail.com>
2025-03-28 23:38:55 +0800

    f

--- scripts/openclash-config/check_start_theme_openclash.sh
+++ scripts/openclash-config/check_start_theme_openclash.sh
@@ -9,20 +9,20 @@ if [ "$CURRENT_THEME" != "argon" ]; then
 fi
 # 定义检查和启动 OpenClash 的函数
 # check_and_start_openclash() {
-#     while true; do
-#         # 检查 OpenClash 服务状态
-#         if /etc/init.d/openclash status | grep -q 'running'; then
-#             echo "OpenClash is running."
-#         else
-#             echo "OpenClash is not running. Starting OpenClash..."
-#             uci set openclash.config.enable=1
-#             uci set openclash.config.config_path='/etc/openclash/config/config.yaml'
-#             uci commit openclash
-#             /etc/init.d/openclash start
-#         fi
-#         # 每隔 60 秒检查一次，可以根据需要调整间隔时间
-#         sleep 10
-#     done
+    # while true; do
+    #     # 检查 OpenClash 服务状态
+    #     if /etc/init.d/openclash status | grep -q 'running'; then
+    #         echo "OpenClash is running."
+    #     else
+    #         echo "OpenClash is not running. Starting OpenClash..."
+    #         uci set openclash.config.enable=1
+    #         uci set openclash.config.config_path='/etc/openclash/config/config.yaml'
+    #         uci commit openclash
+    #         /etc/init.d/openclash start
+    #     fi
+    #     # 每隔 60 秒检查一次，可以根据需要调整间隔时间
+    #     sleep 10
+    # done
 # }
 # # 调用函数
 # check_and_start_openclash  
